bbd7abe41f2d4146876b8e1aa97a489ccb8a82a5
ojjja <oyvind@skidspelen.se>
2022-10-15 18:58:38 +0000

    start adding script

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,12 +4,24 @@ function newGame () {
 }
 
 function userChoice() {
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            if (this.getAttribute("data-type") === "rock") {
+                checkAnswer();
+            } else {
+                let gameType = this.getAttribute("data-type");
+                runGame(gameType);
+            }
 
 }
 
 function computerCoice() {
-
+      // Creates one random numbers between 0 and 2
+      let num1 = Math.floor(Math.random() * 3);
+      console.log(num1)
 }
+
+computerCoice()
 
 function gameResult() {
 
