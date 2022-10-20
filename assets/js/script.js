2d9e7ea2c64a8193263c7e78e27d602a94050e14
ojjja <oyvind@skidspelen.se>
2022-10-20 20:05:31 +0000

    small changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,44 +1,11 @@
 
-function newGame () {
-    
-}
 
-function userChoice() {
-    for (let button of buttons) {
-        button.addEventListener("click", function() {
-            if (this.getAttribute("data choice") === "0") {
-                checkAnswer();
-            } else {
-                let gameType = this.getAttribute("data-type");
-                runGame(gameType);
-            }
+//let picturButton = document.getElementsByClassName ('picture')
 
-}
 
-function computerCoice() {
+
+// function computerCoice() {
       // Creates one random numbers between 0 and 2
-      let num1 = Math.floor(Math.random() * 3);
-      console.log(num1)
-}
-
-computerCoice()
-
-function gameResult() {
-
-}
-
-function gameScore() {
-
-}
-
-function incrementScore() {
-
-}
-
-function gameOver() {
-
-}
-
-function gameStrategi() {
-
-}
+ //     let num1 = Math.floor(Math.random() * 3);
+ //     console.log(num1)
+// }
